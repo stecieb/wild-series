8fffd2f7f5705c391e2db1db6bc7bbcf122edc38
Stecie <stecie.ferriere@gmail.com>
2021-11-30 11:09:31 +0100

    quêtes3

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+Class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/program/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
